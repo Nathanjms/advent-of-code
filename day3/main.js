da9ee634e5f75bfe4dce757b5bf1324947bf1b11
Nathan James <nathanjms@outlook.com>
2023-12-08 21:16:49 +0000

    day 3 partOne

--- day3/main.js
+++ day3/main.js
@@ -1,48 +1,125 @@
 import fs from "fs";
 
-const inputPath = "./day3/example-input";
+const inputPath = "./day3/input";
 
 export function partOne() {
   var input = fs.readFileSync(inputPath, "utf8");
   var inputArray = input.trim().split("\n");
 
-  // Each line has numbers or symbols. We find each symbol, and then find ALL the numbers next to it (including diagonals).
-  // So each line is the same length, so we can use [i][j] syntax to get the symbol at a given position.
-
-  // First we will loop through to find all of the i,j coordinates of the numbers we need.
-  // Then we will extract these. This isn' easy because they may span multiple i coordinates.
-  // Because of this, maybe we should find the LHS-most number for each, then unique them, then extract them?
-
-  const coordinatesOfNumbers = [];
-
-  // The coordinates surrounding any symbol.
-  let coordinatesToCheck = [];
+  const numbersToUse = [];
 
   for (let rowIndex = 0; rowIndex < inputArray.length; rowIndex++) {
     let line = inputArray[rowIndex];
-    for (let j = 0; j < line.length; j++) {
-      // If is any of *,#,+,$, then we need to find the numbers around it:
-      if (["*", "#", "+", "$"].includes(line[j])) console.log("match");
-      coordinatesToCheck = [
-        ...coordinatesToCheck,
-        ...getCoordinatesAroundPoint(rowIndex, j, inputArray.length, line.length),
-      ];
-    }
+    // Go through and find a number
+    let matches = line.matchAll(/\d+/g);
+
+    [...matches].forEach((match) => {
+      let hasSymbol = false;
+      let digitIndex = 0;
+      // Split number to array of digits:
+      let digits = match[0].split("");
+      // For each digit of the number, check the surrounding coordinates for a symbol, until we have either found one, or run out of digits.
+      while (!hasSymbol && digitIndex < digits.length) {
+        // Get the coordinates surrounding the digit:
+        const coordinates = getCoordinatesAroundPoint(
+          rowIndex,
+          match.index + digitIndex,
+          inputArray.length,
+          line.length
+        );
+        // Check if any of these coordinates have a symbol:
+        for (const coordinate of coordinates) {
+          if (inputArray[coordinate[0]][coordinate[1]].match(/[^\s\d\.\w]/)) {
+            hasSymbol = true;
+            break;
+          }
+        }
+        digitIndex++;
+      }
+      // If we have found a symbol, add the number to the list:
+      if (hasSymbol) {
+        numbersToUse.push(Number(match[0]));
+        return;
+      }
+    });
   }
 
-  console.log(coordinatesToCheck);
+  // We could sum as we go, but this helps with debugging, so reduce the array to a sum:
+  console.log(
+    "Sum: ",
+    numbersToUse.reduce((a, b) => a + b, 0)
+  );
+}
 
-  function getCoordinatesAroundPoint(i, j, maxI, maxJ) {
-    const coordinates = [];
-    // 8 Possible coordinates around a point, loop these and check if they are valid:
-    for (let x = i - 1; x <= i + 1; x++) {
-      for (let y = j - 1; y <= j + 1; y++) {
-        // If the coordinate is valid, add it to the list:
-        if (x >= 0 && x < maxI && y >= 0 && y < maxJ) {
-          coordinates.push([x, y]);
+export function partTwo() {
+  var input = fs.readFileSync(inputPath, "utf8");
+  var inputArray = input.trim().split("\n");
+
+  const gearRatios = [];
+
+  // We now need to find any * characters, and check if they are surrounded by numbers. If they are surrounded by MORE THAN ONE, store these numbers
+  for (let rowIndex = 0; rowIndex < inputArray.length; rowIndex++) {
+    let line = inputArray[rowIndex];
+    // Get the *'s on this line:
+    let matches = line.matchAll(/\*/g);
+
+    const numbersByFirstCoordinate = {};
+    [...matches].forEach((match) => {
+      // Get the coordinates surrounding the *:
+      const coordinates = getCoordinatesAroundPoint(rowIndex, match.index, inputArray.length, line.length);
+      // Check if any of these coordinates have a number:
+      for (const coordinate of coordinates) {
+        if (inputArray[coordinate[0]][coordinate[1]].match(/\d/)) {
+          // If we have a digit, we need to get the full number from it by going left and right until there is not a digit:
+          let leftDigitIndex = coordinate[1] - 1;
+          let number = inputArray[coordinate[0]][coordinate[1]];
+          // Go left:
+          while (leftDigitIndex >= 0 && inputArray[coordinate[0]][leftDigitIndex].match(/\d/)) {
+            number = inputArray[coordinate[0]][leftDigitIndex].toString() + number.toString();
+            leftDigitIndex--;
+          }
+          // Go right:
+          let rightDigitIndex = coordinate[1] + 1;
+          while (rightDigitIndex < line.length && inputArray[coordinate[0]][rightDigitIndex].match(/\d/)) {
+            number = number.toString() + inputArray[coordinate[0]][rightDigitIndex].toString();
+            rightDigitIndex++;
+          }
+
+          // If this number is not already in the list, add it:
+          if (!numbersByFirstCoordinate.hasOwnProperty((leftDigitIndex + 1).toString())) {
+            numbersByFirstCoordinate[(leftDigitIndex + 1).toString()] = Number(number);
+          }
         }
       }
+    });
+
+    // if there are exactly two numbers, add their product to the list:
+    if (Object.keys(numbersByFirstCoordinate).length === 2) {
+      gearRatios.push(
+        numbersByFirstCoordinate[Object.keys(numbersByFirstCoordinate)[0]] *
+          numbersByFirstCoordinate[Object.keys(numbersByFirstCoordinate)[1]]
+      );
     }
-    return coordinates;
   }
+
+  console.log({ gearRatios });
+
+  console.log(
+    "Sum: ",
+    gearRatios.reduce((a, b) => a + b, 0)
+  );
+}
+
+function getCoordinatesAroundPoint(i, j, maxI, maxJ) {
+  const coordinates = [];
+  // 8 Possible coordinates around a point, loop these and check if they are valid:
+  for (let x = i - 1; x <= i + 1; x++) {
+    for (let y = j - 1; y <= j + 1; y++) {
+      // If the coordinate is valid, add it to the list:
+      if (x >= 0 && x < maxI && y >= 0 && y < maxJ) {
+        coordinates.push([x, y]);
+      }
+    }
+  }
+  return coordinates;
 }
--- main.js
+++ main.js
@@ -2,12 +2,12 @@ import * as dayOne from "./day1/main.js";
 import * as dayTwo from "./day2/main.js";
 import * as dayThree from "./day3/main.js";
 
-console.log("Day 1:");
+console.log("---------Day 1-----------");
 dayOne.partOne();
 dayOne.partTwo();
-console.log("--------------------");
+console.log("---------Day 2-----------");
 dayTwo.partOne();
 dayTwo.partTwo();
-console.log("--------------------");
+console.log("---------Day 3-----------");
 dayThree.partOne();
-
+dayThree.partTwo();
