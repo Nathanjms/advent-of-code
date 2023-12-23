b794e5f1e29617de191f42a70799cdd346dcb4d7
Nathan James <nathanjms@outlook.com>
2023-12-23 09:58:33 +0000

    Day 23 pt 1 - Learned about Edge Contraction
    DOn't think I would have goot this with brute force, was taking too long

---
+++ day23/main.js
@@ -0,0 +1,158 @@
+import fs from "fs";
+
+const inputPath = "./day23/example-input";
+
+const DIRECTIONS = {
+  LEFT: "left",
+  RIGHT: "right",
+  UP: "up",
+  DOWN: "down",
+};
+
+const SLOPE_TO_DIRECTION = {
+  "^": DIRECTIONS.UP,
+  ">": DIRECTIONS.RIGHT,
+  v: DIRECTIONS.DOWN,
+  "<": DIRECTIONS.LEFT,
+};
+
+export function partOne(input = null) {
+  var input = input || inputPath;
+  input = fs.readFileSync(input, "utf8");
+  var trailMap = input
+    .trim()
+    .split("\n")
+    .map((line) => line.split(""));
+
+  let start = [0, trailMap[0].indexOf(".")];
+  let end = [trailMap.length - 1, trailMap[trailMap.length - 1].indexOf(".")];
+
+  /* Use Edge Contraction to simplify the input first, before using brute force to find the longest path */
+
+  let points = [[...start], [...end]]; // STart with the 'points of interest' as just the start and end
+  for (let r = 0; r < trailMap.length; r++) {
+    for (let c = 0; c < trailMap[r].length; c++) {
+      const point = trailMap[r][c];
+      if (point === "#") {
+        continue;
+      }
+      let neighbours = 0;
+      for (const dir of Object.values(DIRECTIONS)) {
+        // If we are currently on a 'Peak', restrict
+        if (["^", ">", "v", "<"].includes(trailMap[r][c]) && dir !== SLOPE_TO_DIRECTION[trailMap[r][c]]) {
+          continue;
+        }
+        let newCoord = step(dir, r, c);
+        if (
+          newCoord[0] >= 0 &&
+          newCoord[0] <= trailMap.length - 1 &&
+          newCoord[1] >= 0 &&
+          newCoord[1] < trailMap[0].length - 1 &&
+          trailMap[newCoord[0]][newCoord[1]] !== "#"
+        ) {
+          neighbours++;
+        }
+      }
+      // If we have at least 2 places to go (+ 1 because we've stepped from a valid point), then this is an 'edge' point
+      if (neighbours >= 3) {
+        points.push([r, c]);
+      }
+    }
+  }
+
+  let graph = points.reduce((agg, a) => {
+    agg[a.join(",")] = [];
+    return agg;
+  }, {});
+
+  // Now we have a list of points that are where the 'splits' occur. Now we can build a graph of a key f the start, and a set of vals
+  // that each contain the number of steps if go that route. Use depths first search for this for each point
+
+  for (const pt of points) {
+    let queue = [{ pt, numSteps: 0 }];
+    let seen = new Set([pt.join(",")]);
+
+    while (queue.length) {
+      const el = queue.pop();
+      const numSteps = el.numSteps;
+      const [newR, newC] = [...el.pt];
+      const key = newR + "," + newC;
+
+      if (numSteps !== 0 && points.some((v) => v[0] == newR && v[1] == newC)) {
+        graph[pt.join(",")].push({ next: [newR, newC], numSteps });
+        continue;
+      }
+
+      for (const dir of Object.values(DIRECTIONS)) {
+        // If we are currently on a 'Peak', restrict
+        if (["^", ">", "v", "<"].includes(trailMap[newR][newC]) && dir !== SLOPE_TO_DIRECTION[trailMap[newR][newC]]) {
+          continue;
+        }
+        let newCoord = step(dir, newR, newC);
+        if (
+          !seen.has(newCoord[0] + "," + newCoord[1]) &&
+          newCoord[0] >= 0 &&
+          newCoord[0] <= trailMap.length - 1 &&
+          newCoord[1] >= 0 &&
+          newCoord[1] < trailMap[0].length - 1 &&
+          trailMap[newCoord[0]][newCoord[1]] !== "#"
+        ) {
+          queue.push({ pt: [...newCoord], numSteps: numSteps + 1 });
+          seen.add(key);
+        }
+      }
+    }
+  }
+
+  // Now we simply start at the start and do a DFS for each paths we can take, outputting the maximum one!
+  const result = depthFirstSearch(...start);
+
+  console.log({ day: 23, part: 1, value: result });
+
+  function depthFirstSearch(i, j) {
+    if (i == end[0] && j == end[1]) {
+      return 0; // At the end so no steps to take
+    }
+
+    let steps = -Infinity;
+
+    for (let { next, numSteps } of graph[i + "," + j]) {
+      steps = Math.max(steps, depthFirstSearch(...next) + numSteps);
+    }
+
+    return steps;
+  }
+}
+
+export function partTwo(input = null) {
+  var input = input || inputPath;
+  input = fs.readFileSync(input, "utf8");
+  var inputArray = input
+    .trim()
+    .split("\n")
+    .map((line) => line.split(""));
+
+  console.log({ day: 23, part: 2, value: "todo" });
+}
+
+function getOppositeDirection(direction) {
+  if (direction === DIRECTIONS.LEFT) return DIRECTIONS.RIGHT;
+  if (direction === DIRECTIONS.RIGHT) return DIRECTIONS.LEFT;
+  if (direction === DIRECTIONS.UP) return DIRECTIONS.DOWN;
+  if (direction === DIRECTIONS.DOWN) return DIRECTIONS.UP;
+}
+
+function step(direction, i, j) {
+  if (direction === DIRECTIONS.RIGHT) {
+    return [i, j + 1];
+  }
+  if (direction === DIRECTIONS.LEFT) {
+    return [i, j - 1];
+  }
+  if (direction === DIRECTIONS.DOWN) {
+    return [i + 1, j];
+  }
+  if (direction === DIRECTIONS.UP) {
+    return [i - 1, j];
+  }
+}
--- main.js
+++ main.js
@@ -20,6 +20,7 @@ import * as day19 from "./day19/main.js";
 import * as day20 from "./day20/main.js";
 import * as day21 from "./day21/main.js";
 import * as day22 from "./day22/main.js";
+import * as day23 from "./day23/main.js";
 
 day01.partOne();
 day01.partTwo();
@@ -68,3 +69,5 @@ day21.partOne();
 console.log({ day: 21, part: 2, value: "Input and Manual Run Required" }); // Input must be day 21's for this to work
 day22.partOne();
 day22.partTwo();
+day23.partOne("input");
+day23.partTwo();
