ff8080bf9bd0f7662b84c6abcb352855579ddf15
Nathan James <nathanjms@outlook.com>
2024-12-11 08:44:13 +0000

    2024 day 11 pt 2
    And refactor pt 1 to use recursive function too

--- 2024/go/day11/main.go
+++ 2024/go/day11/main.go
@@ -33,114 +33,90 @@ func main() {
 	partTwo(contents)
 }
 
+type cacheKey struct {
+	rock            int64
+	remainingBlinks int
+}
+
 func partOne(contents string) {
 	rocks := parseInput(contents)
 
-	numBlinks := 25
+	cacheMap := make(map[cacheKey]int64, 0)
 
-	for i := 0; i < numBlinks; i++ {
-		newLayout := make([]int64, 0)
-
-		for _, rock := range rocks {
-			if rock == 0 {
-				newLayout = append(newLayout, int64(1))
-			} else {
-				stringVal := strconv.FormatInt(rock, 10)
-				stringValSize := len(stringVal)
-				if stringValSize%2 == 0 {
-					// Split down the middle;
-					var newRockOneStr, newRockTwoStr string
-					var newRockOne, newRockTwo int64
-					// Special case if size is 2:
-					if stringValSize == 2 {
-						newRockOneStr = string(stringVal[0])
-						newRockTwoStr = string(stringVal[1])
-					} else {
-						newRockOneStr = stringVal[0:(stringValSize / 2)]
-						newRockTwoStr = stringVal[(stringValSize / 2):stringValSize]
-					}
-
-					newRockOne, _ = strconv.ParseInt(newRockOneStr, 10, 64)
-					newRockTwo, _ = strconv.ParseInt(newRockTwoStr, 10, 64)
-
-					newLayout = append(newLayout, newRockOne)
-					newLayout = append(newLayout, newRockTwo)
-				} else {
-					newLayout = append(newLayout, rock*2024)
-				}
-			}
-		}
-
-		rocks = make([]int64, len(newLayout))
-		copy(rocks, newLayout)
+	numRocks := int64(0)
+	for _, rock := range rocks {
+		numRocks += getNumberOfRocks(rock, 25, &cacheMap)
 	}
+
 	sharedstruct.PrintOutput(sharedstruct.Output{
 		Day:   11,
 		Part:  1,
-		Value: len(rocks),
+		Value: numRocks,
 	})
 }
 
 func partTwo(contents string) {
 	rocks := parseInput(contents)
 
-	numBlinks := 75
+	cacheMap := make(map[cacheKey]int64, 0)
 
-	// Store the output from input single digit, what it becomes
-	cacheMap := make(map[int64][]int64, 400000)
-
-	// Recursion makes sense?
-	numRocks := 0
+	numRocks := int64(0)
 	for _, rock := range rocks {
-		numRocks += getNumberOfRocks(rock, &cacheMap, 75)
+		numRocks += getNumberOfRocks(rock, 75, &cacheMap)
 	}
 
 	sharedstruct.PrintOutput(sharedstruct.Output{
 		Day:   11,
 		Part:  2,
-		Value: len(rocks),
+		Value: numRocks,
 	})
 }
 
-func getNumberOfRocks(rock int64, cacheMap *map[int64][]int64, blinks int) int64 {
-	val, ok := (*cacheMap)[rock]
+func getNumberOfRocks(rock int64, blinksRemaining int, cacheMap *map[cacheKey]int64) int64 {
+	val, ok := (*cacheMap)[cacheKey{rock, blinksRemaining}]
 	if ok {
-		for _, newRock := range val {
-			numRocks += getNumberOfRocks(newRock, cacheMap, numRocks)
-		}
-
-		return numRocks
+		return val
 	}
-	// Otherwise, do it then store in cache
+
+	if blinksRemaining == 0 {
+		return 1 // Just this rock
+	}
+
 	if rock == 0 {
-		newRock = 1
-		(*cacheMap)[rock] = 1
-	} else {
-		stringVal := strconv.FormatInt(rock, 10)
-		stringValSize := len(stringVal)
-		if stringValSize%2 == 0 {
-			// Split down the middle;
-			var newRockOneStr, newRockTwoStr string
-			var newRockOne, newRockTwo int64
-			// Special case if size is 2:
-			if stringValSize == 2 {
-				newRockOneStr = string(stringVal[0])
-				newRockTwoStr = string(stringVal[1])
-			} else {
-				newRockOneStr = stringVal[0:(stringValSize / 2)]
-				newRockTwoStr = stringVal[(stringValSize / 2):stringValSize]
-			}
-
-			newRockOne, _ = strconv.ParseInt(newRockOneStr, 10, 64)
-			newRockTwo, _ = strconv.ParseInt(newRockTwoStr, 10, 64)
-
-			// Add to cache;
-			cacheMap[rock] = [2]int64{newRockOne, newRockTwo}
-		} else {
-			value := rock * 2024
-			cacheMap[rock] = [2]int64{value, -1}
-		}
+		value := getNumberOfRocks(int64(1), blinksRemaining-1, cacheMap)
+		(*cacheMap)[cacheKey{rock, blinksRemaining}] = value
+		return value
 	}
+
+	stringVal := strconv.FormatInt(rock, 10)
+	stringValSize := len(stringVal)
+	if stringValSize%2 == 0 {
+		// Split down the middle;
+		var newRockOneStr, newRockTwoStr string
+		var newRockOne, newRockTwo int64
+		// Special case if size is 2:
+		if stringValSize == 2 {
+			newRockOneStr = string(stringVal[0])
+			newRockTwoStr = string(stringVal[1])
+		} else {
+			newRockOneStr = stringVal[0:(stringValSize / 2)]
+			newRockTwoStr = stringVal[(stringValSize / 2):stringValSize]
+		}
+
+		newRockOne, _ = strconv.ParseInt(newRockOneStr, 10, 64)
+		newRockTwo, _ = strconv.ParseInt(newRockTwoStr, 10, 64)
+
+		value := getNumberOfRocks(newRockOne, blinksRemaining-1, cacheMap) + getNumberOfRocks(newRockTwo, blinksRemaining-1, cacheMap)
+
+		(*cacheMap)[cacheKey{rock, blinksRemaining}] = value
+
+		return value
+	}
+
+	value := getNumberOfRocks(rock*2024, blinksRemaining-1, cacheMap)
+	(*cacheMap)[cacheKey{rock, blinksRemaining}] = value
+	return value
+
 }
 
 func parseInput(contents string) []int64 {
