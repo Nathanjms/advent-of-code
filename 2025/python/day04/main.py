e4482350104dc2ef7125e5913190127413a5570a
Nathan James <nathanjms@outlook.com>
2025-12-04 08:08:01 +0000

    part 2

--- 2025/python/day04/main.py
+++ 2025/python/day04/main.py
@@ -42,7 +42,31 @@ def part_one(lines: list[str]) -> int:
 
 
 def part_two(lines: list[str]) -> int:
-    return 0
+    removed_rolls_count = 0
+
+    # We keep repeating until there's no more to add, where we break
+    while True:
+        can_be_removed_coordinates: dict = {}
+        for i, line in enumerate(lines):
+            for j in range(len(line)):
+                if lines[i][j] == "@" and check_surroundings(i, j, lines):
+                    can_be_removed_coordinates[str(i) + "," + str(j)] = True
+        if len(can_be_removed_coordinates) > 0:
+            removed_rolls_count += len(can_be_removed_coordinates)
+
+            # Now we rebuild the lines 'grid'.
+            for i, line in enumerate(lines):
+                line_tmp = []
+                for j in range(len(line)):
+                    if can_be_removed_coordinates.get(str(i) + "," + str(j)):
+                        line_tmp.append(".")
+                    else:
+                        line_tmp.append(lines[i][j])
+                lines[i] = "".join(line_tmp)
+        else:
+            break
+
+    return removed_rolls_count
 
 
 if __name__ == "__main__":
